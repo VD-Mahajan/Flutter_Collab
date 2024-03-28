f5d6c869b32a7354c8418d8c51ef75a23200014d
Vishal Datta Mahajan <mahajanvishal410a@gmail.com>
2024-03-28 22:21:33 +0530

    Connectivity code commented

--- my_agri_app/lib/dependency_injection.dart
+++ my_agri_app/lib/dependency_injection.dart
@@ -1,8 +1,8 @@
-import 'package:get/get.dart';
-import 'package:my_agri_app/controller/nertwork_controller.dart';
+// import 'package:get/get.dart';
+// import 'package:my_agri_app/controller/nertwork_controller.dart';
 
-class DependencyInjection {
-  static void init() {
-    Get.put<NetworkController>(NetworkController(), permanent: true);
-  }
-}
+// class DependencyInjection {
+//   static void init() {
+//     Get.put<NetworkController>(NetworkController(), permanent: true);
+//   }
+// }
